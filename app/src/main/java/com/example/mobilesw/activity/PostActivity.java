1f3da008ffaf8ac6fc8edd87f9caebf808b7d3a0
danilkim99 <43752891+danilkim99@users.noreply.github.com>
2021-05-29 05:36:53 +0900

    했던 부분을 아직 마무리 못 했지만 여기까지 하엿습니다. 왠지 가상 기계가 실행되지 않아서 호확을 못 하였습니다. 저는 코드를 따로 해서 따로 있는 폴도 경우는 잘 동작됩니다.

---
+++ app/src/main/java/com/example/mobilesw/activity/PostActivity.java
@@ -0,0 +1,184 @@
+package com.example.mobilesw.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobilesw.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class PostActivity extends AppCompatActivity {
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+
+    private static final int Gallery_Pick = 1;
+    private Uri ImageUri;
+    private String Description;
+
+    private StorageReference PostsImagesReference;
+    private DatabaseReference UsersRef, PostsRef;
+    private FirebaseAuth mAuth;
+
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName, downloadUrl, current_user_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        mAuth = FirebaseAuth.getInstance();
+        current_user_id = mAuth.getCurrentUser().getUid();
+
+        PostsImagesReference = FirebaseStorage.getInstance().getReference();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+
+        SelectPostImage = (ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription = (EditText) findViewById(R.id.post_description);
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenGallery();
+            }
+        });
+
+        UpdatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValidatePostInfo();
+            }
+        });
+    }
+
+    //if something is not uploaded help message
+    private void ValidatePostInfo() {
+        Description = PostDescription.getText().toString();
+
+        if(ImageUri==null){
+            Toast.makeText(this, "Please select post image", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Description)){
+            Toast.makeText(this, "Please write something about your post", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            StoringImageToFirebaseStorage();
+        }
+    }
+
+    //sorting image to database
+    private void StoringImageToFirebaseStorage() {
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-mm-yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        Calendar calForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        postRandomName = saveCurrentDate+saveCurrentTime;
+
+        StorageReference filePath = PostsImagesReference.child("Post Images").child(ImageUri.getLastPathSegment() + postRandomName + ".jpg");
+
+        filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+
+                    downloadUrl = task.getResult().getMetadata().getReference().getDownloadUrl().toString();
+                    Toast.makeText(PostActivity.this, "Image uploaded successfully to Storage", Toast.LENGTH_SHORT).show();
+
+                    SavingPostInformationToDatabase();
+                }
+                else{
+                    String message = task.getException().getMessage();
+                    Toast.makeText(PostActivity.this, "Error occured" + message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    /////////////////////p유튜브 있는 영상에 따라 해 봤는데 구조가 달라서 데이터배이스에 정보가 안 나옵니다 ㅜㅜㅜ
+    private void SavingPostInformationToDatabase() {
+        UsersRef.child(current_user_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String userFullName = dataSnapshot.child("fullname").getValue().toString();
+                    String userProfileImage = dataSnapshot.child("profile image").getValue().toString();
+
+                    HashMap postsMap = new HashMap();
+                    postsMap.put("uid", current_user_id);
+                    postsMap.put("date", saveCurrentDate);
+                    postsMap.put("time", saveCurrentTime);
+                    postsMap.put("description", Description);
+                    postsMap.put("postimage", downloadUrl);
+                    postsMap.put("profileimage", userProfileImage);
+                    postsMap.put("fullname", userFullName);
+                    PostsRef.child(postRandomName).updateChildren(postsMap)
+                            .addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task) {
+                                    if(task.isSuccessful()){
+                                        //   SendUserToFragBoard();
+                                        Toast.makeText(PostActivity.this, "Post is updated successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(PostActivity.this, "Occured while updating post", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    //gallery connect
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
+
+    }
+    //save image on screen while posting
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==Gallery_Pick && resultCode==RESULT_OK && data!=null){
+            ImageUri = data.getData();
+            SelectPostImage.setImageURI(ImageUri);}
+    }
+
+}
